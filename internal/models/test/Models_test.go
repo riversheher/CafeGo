0db2ab8b513ee477a2b897a9af46480499d05d58
River Wang <personal@rainbowriverrr.com>
2023-07-09 22:14:09 -0400

    utilizing TestMain to initialize database connection

--- internal/models/test/Models_test.go
+++ internal/models/test/Models_test.go
@@ -14,13 +14,39 @@ const (
 	testDB = "test"
 )
 
+var (
+	db     *sql.DB
+	tables = []string{
+		models.IngredientTable,
+		models.MenuTable,
+		models.ProductTable,
+		models.UserTable,
+		models.AdminTable,
+		models.OrderTable,
+		models.AlternativesTable,
+		models.ProductToMenuTable,
+		models.ProductToOrderTable,
+		models.IngredientToProductTable,
+	}
+)
+
+func TestMain(m *testing.M) {
+	db = database.InitDB(testDB)
+	defer db.Close()
+
+	m.Run()
+
+	clean()
+}
+
+func clean() {
+	//delete test database
+	db.Close()
+	database.DeleteDB(testDB)
+}
+
 // TestInitTables tests the InitTables function
 func TestInitTables(t *testing.T) {
-	var db *sql.DB = database.InitDB(testDB)
-
-	defer db.Close()
-	defer clean()
-
 	models.InitTables(db)
 
 	query := "SELECT name FROM sqlite_schema WHERE type='table' AND name NOT  LIKE 'sqlite_%';"
@@ -42,28 +68,18 @@ func TestInitTables(t *testing.T) {
 	}
 
 	// check if tables exist
-	tables := []string{
-		models.IngredientTable,
-		models.MenuTable,
-		models.ProductTable,
-		models.UserTable,
-		models.AdminTable,
-		models.OrderTable,
-		models.AlternativesTable,
-		models.ProductToMenuTable,
-		models.ProductToOrderTable,
-		models.IngredientToProductTable,
-	}
-
 	for _, table := range tables {
 		_, err := db.Query(fmt.Sprintf("SELECT * FROM %s", table))
 		if err != nil {
 			t.Errorf("Error querying table %s: %s", table, err.Error())
 		}
 	}
-}
 
-func clean() {
-	//delete test database
-	database.DeleteDB(testDB)
+	//drop all tables
+	for _, table := range tables {
+		_, err := db.Exec(fmt.Sprintf("DROP TABLE %s", table))
+		if err != nil {
+			t.Errorf("Error dropping table %s: %s", table, err.Error())
+		}
+	}
 }
--- internal/models/test/User_test.go
+++ internal/models/test/User_test.go
@@ -3,13 +3,47 @@ package models_test
 import (
 	"testing"
 
-	"github.com/rainbowriverrr/CafeGo/pkg/database"
+	"github.com/rainbowriverrr/CafeGo/internal/models"
 	_ "modernc.org/sqlite"
 )
 
-func TestCreateUserTables(t *testing.T) {
-	db := database.InitDB(testDB)
+func init() {
 
-	defer db.Close()
-	defer clean()
+}
+
+func TestCreateUserTables(t *testing.T) {
+	models.CreateUserTables(db)
+
+	//check if table exists
+	query := "SELECT name FROM sqlite_schema WHERE type='table' AND name = ?;"
+	rows, err := db.Query(query, models.UserTable)
+	if err != nil {
+		t.Errorf("Error querying database: %s", err.Error())
+	}
+
+	defer rows.Close()
+
+	if rows.Next() == false {
+		t.Errorf("Table %s does not exist", models.UserTable)
+	}
+}
+
+func TestUserExists(t *testing.T) {
+	app := models.Application{
+		DB: db,
+	}
+
+	//create table and insert user
+	models.CreateUserTables(db)
+	user := models.User{
+		Name:  "test",
+		Phone: "1234567890",
+	}
+	app.InsertUser(user)
+
+	//check if user exists
+	exists := app.UserExists(user)
+	if exists == false {
+		t.Errorf("User %s does not exist", user.Name)
+	}
 }
